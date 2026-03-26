7e4c589edd0dd0d15607ff1d9a3f6f8553737d1d
Vova-81 <naumovvova02@gmail.com>
2026-03-26 21:29:20 +0300

    Update server.js

--- HOST/server/server.js
+++ HOST/server/server.js
@@ -30,7 +30,7 @@ const pool = new Pool({
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Создание таблиц
 async function initDatabase() {
